8cfa7a13faa2bba2b17d5efb15945bea6297c7af
Ivan Ushakov <ivan@ushakov.tech>
2024-05-30 11:34:29 +0400

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "YandexMapsMobileLite",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "YandexMapsMobile",
+            targets: ["YandexMapsMobile"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "YandexMapsMobile",
+            url: "",
+            checksum: ""
+        ),
+    ]
+)
