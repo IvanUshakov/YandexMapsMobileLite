92558dc979b69bb21d77f6e5809d3c1b755634a6
Ivan Ushakov <ivan@ushakov.tech>
2024-05-30 11:41:24 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YandexMapsMobile",
-            url: "",
-            checksum: ""
+            url: "https://github.com/IvanUshakov/YandexMapsMobileLite/releases/download/4.6.1/YandexMapsMobile.xcframework.zip",
+            checksum: "641965e4dbb6c30eff55da5af4c91ee723ee90bdf582dcf613d677f82fd0d56d"
         ),
     ]
 )
